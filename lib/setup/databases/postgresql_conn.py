0118d09bbb6fb671aee94edba10f4a4c11916d21
Carlos Eduardo Smaniotto <carlos.smaniotto@datapower.com.br>
2013-11-18 18:43:48 -0200

    PostgreSQL Support

--- lib/setup/databases/postgresql_conn.py
+++ lib/setup/databases/postgresql_conn.py
@@ -68,7 +68,111 @@ class PostgreSQL_Connect(object):
         version_list[version.strip('.')[1], version.strip('.')[2], version.strip('.')[3]]
         return tuple(version_list)
 
+    ''' Show postgresql.conf '''
+
     def pgsql_show_all(self):
         pg_version = self.pgsql_verion_tuple()
         rs = self.pgsql_execute("show all")
         return conf_anysize2byte(rs, pg_version)
+
+    ''' Another way to show postgresql.conf '''
+
+    def pgsql_pg_setting(self):
+        QUERY = "SELECT name, setting FROM pg_settings;"
+        rs = self.pgsql_execute(QUERY)
+        return rs
+
+    def pgsql_autovacuum_freeze_max_age(self):
+
+        '''
+          type              => 'percent',
+          default_warning   => '90%',
+          default_critical  => '95%',
+          normal <=             '25%'
+        '''
+        QUERY = """
+                SELECT freez,
+                      txns,
+                      ROUND(100*(txns/freez::float)) AS perc,
+                      datname FROM (
+                                    SELECT foo.freez::int,
+                                           age(datfrozenxid) AS txns,
+                                           datname
+                                    FROM pg_database d
+                                    JOIN (SELECT setting AS freez FROM pg_settings WHERE name = 'autovacuum_freeze_max_age') AS foo
+                                    ON (true) WHERE d.datallowconn
+                                   )
+                AS foo2 ORDER BY 3 DESC, 4 ASC;
+            """
+        status = self.pgsql_execute(QUERY)
+        return status
+
+    def pgsql_last_vacuum(self):
+        status = None
+        '''
+        ltime = second between last and now()
+        ptime = time of last
+        '''
+        QUERY = """
+                SELECT  current_database() AS datname,
+	                    nspname AS sname,
+	                    relname AS tname,
+                        CASE WHEN v IS NULL THEN
+                                -1
+                        ELSE
+                            round(extract(epoch FROM now()-v))
+                        END AS ltime,
+                        CASE WHEN v IS NULL THEN
+                            '?'
+                        ELSE
+                            TO_CHAR(v, 'HH24:MI FMMonth DD, YYYY')
+                        END AS ptime
+                FROM (
+                        SELECT  nspname,
+                                relname,
+                                GREATEST(pg_stat_get_last_vacuum_time(c.oid), pg_stat_get_last_autovacuum_time(c.oid)) AS v
+                        FROM pg_class c, pg_namespace n
+                        WHERE relkind = 'r'
+                        AND n.oid = c.relnamespace
+                        AND n.nspname <> 'information_schema'
+                        ORDER BY 3
+                    ) AS foo
+                """
+        status = self.pgsql_execute(QUERY)
+        return status
+
+    def pgsql_last_analyze(self):
+        status = None
+        '''
+        ltime = second between last and now()
+        ptime = time of last
+        '''
+        QUERY = """
+                SELECT  current_database() AS datname,
+	                    nspname AS sname,
+	                    relname AS tname,
+                        CASE WHEN v IS NULL THEN
+                                -1
+                        ELSE
+                            round(extract(epoch FROM now()-v))
+                        END AS ltime,
+                        CASE WHEN v IS NULL THEN
+                            '?'
+                        ELSE
+                            TO_CHAR(v, 'HH24:MI FMMonth DD, YYYY')
+                        END AS ptime
+                FROM (
+                        SELECT  nspname,
+                                relname,
+                                GREATEST(pg_stat_get_last_analyze_time(c.oid), pg_stat_get_last_autoanalyze_time(c.oid)) AS v
+                        FROM pg_class c, pg_namespace n
+                        WHERE relkind = 'r'
+                        AND n.oid = c.relnamespace
+                        AND n.nspname <> 'information_schema'
+                        ORDER BY 3
+                    ) AS foo
+            """
+        status = self.pgsql_execute(QUERY)
+        return status
+
+
